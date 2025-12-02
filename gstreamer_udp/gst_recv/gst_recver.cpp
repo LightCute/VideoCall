24271aa028c633389ad520d04f56beb45ed80f9a
Light <13729789313@163.com>
2025-12-02 16:21:13 +0800

    Light_25_12_2_MJPEG_UDP_2

--- gstreamer_udp/gst_recv/gst_recver.cpp
+++ gstreamer_udp/gst_recv/gst_recver.cpp
@@ -1,83 +1,65 @@
 #include <gst/gst.h>
 
 int main(int argc, char *argv[]) {
-    GstElement *pipeline, *udpsrc, *rtpjpegdepay, *jpegdec, *videoconvert, *videosink;
+    GstElement *pipeline, *udpsrc, *rtpjpegdepay, *jpegparse, *jpegdec, *videoconvert, *videosink;
     GstBus *bus;
     GstMessage *msg;
     GstStateChangeReturn ret;
 
     gst_init(&argc, &argv);
 
-    /* Create elements */
-    udpsrc        = gst_element_factory_make("udpsrc", "udpsrc");
-    rtpjpegdepay  = gst_element_factory_make("rtpjpegdepay", "rtpjpegdepay");
-    jpegdec       = gst_element_factory_make("jpegdec", "jpegdec");
-    videoconvert  = gst_element_factory_make("videoconvert", "videoconvert");
-    videosink     = gst_element_factory_make("xvimagesink", "videosink");
+    udpsrc       = gst_element_factory_make("udpsrc", "udpsrc");
+    rtpjpegdepay = gst_element_factory_make("rtpjpegdepay", "rtpjpegdepay");
+    jpegparse    = gst_element_factory_make("jpegparse", "jpegparse");
+    jpegdec      = gst_element_factory_make("jpegdec", "jpegdec");
+    videoconvert = gst_element_factory_make("videoconvert", "videoconvert");
+    videosink    = gst_element_factory_make("xvimagesink", "videosink");
 
-    if (!udpsrc || !rtpjpegdepay || !jpegdec || !videoconvert || !videosink) {
-        g_printerr("Failed to create some elements.\n");
+    if (!udpsrc || !rtpjpegdepay || !jpegparse || !jpegdec || !videoconvert || !videosink) {
+        g_printerr("Failed to create elements.\n");
         return -1;
     }
 
-    /* Create pipeline */
-    pipeline = gst_pipeline_new("mjpeg-recv-pipeline");
-    if (!pipeline) {
-        g_printerr("Failed to create pipeline.\n");
-        return -1;
-    }
+    pipeline = gst_pipeline_new("mjpeg-rtp-recv");
 
-    /* Set udpsrc port */
     g_object_set(udpsrc, "port", 5000, NULL);
 
-    /* Set MJPEG RTP caps */
     GstCaps *caps = gst_caps_new_simple("application/x-rtp",
-                                        "media", G_TYPE_STRING, "video",
-                                        "encoding-name", G_TYPE_STRING, "JPEG",
-                                        "payload", G_TYPE_INT, 26,
-                                        NULL);
-
+        "media", G_TYPE_STRING, "video",
+        "encoding-name", G_TYPE_STRING, "JPEG",
+        "payload", G_TYPE_INT, 26,
+        NULL);
     g_object_set(udpsrc, "caps", caps, NULL);
     gst_caps_unref(caps);
 
-    /* Add all elements */
     gst_bin_add_many(GST_BIN(pipeline),
-                      udpsrc, rtpjpegdepay, jpegdec, videoconvert, videosink,
-                      NULL);
+        udpsrc, rtpjpegdepay, jpegparse, jpegdec, videoconvert, videosink, NULL);
 
-    /* Link them */
-    if (!gst_element_link_many(udpsrc, rtpjpegdepay, jpegdec, videoconvert, videosink, NULL)) {
-        g_printerr("Failed to link elements!\n");
-        gst_object_unref(pipeline);
+    if (!gst_element_link_many(udpsrc, rtpjpegdepay, jpegparse, jpegdec, videoconvert, videosink, NULL)) {
+        g_printerr("Failed to link elements.\n");
         return -1;
     }
 
-    /* Start pipeline */
     ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
     if (ret == GST_STATE_CHANGE_FAILURE) {
         g_printerr("Unable to set pipeline to PLAYING.\n");
-        gst_object_unref(pipeline);
         return -1;
     }
 
-    /* Wait for error/EOS */
     bus = gst_element_get_bus(pipeline);
-    msg = gst_bus_timed_pop_filtered(
-        bus, GST_CLOCK_TIME_NONE,
-        (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+        GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
-    /* Handle message */
     if (msg) {
         GError *err;
-        gchar *debug_info;
-        gst_message_parse_error(msg, &err, &debug_info);
-        g_printerr("Error received: %s\n", err->message);
+        gchar *debug;
+        gst_message_parse_error(msg, &err, &debug);
+        g_printerr("Error: %s\n", err->message);
         g_error_free(err);
-        g_free(debug_info);
+        g_free(debug);
         gst_message_unref(msg);
     }
 
-    /* Cleanup */
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(bus);
     gst_object_unref(pipeline);
--- gstreamer_udp/gst_send/gst_sender.cpp
+++ gstreamer_udp/gst_send/gst_sender.cpp
@@ -1,58 +1,45 @@
 #include <gst/gst.h>
 
 int main(int argc, char *argv[]) {
-    GstElement *pipeline, *v4l2src, *capsfilter, *tee, *queue1, *queue2;
-    GstElement *jpegdec, *videoconvert, *rtpjpegpay, *udpsink, *autovideosink;
+    GstElement *pipeline, *v4l2src, *capsfilter;
+    GstElement *tee, *queue_display, *queue_network;
+    GstElement *jpegdec, *videoconvert, *autovideosink;
+    GstElement *rtpjpegpay, *udpsink;
     GstBus *bus;
     GstMessage *msg;
     GstStateChangeReturn ret;
 
     gst_init(&argc, &argv);
 
-    /* Create elements */
-    v4l2src = gst_element_factory_make("v4l2src", "v4l2src");
-    capsfilter = gst_element_factory_make("capsfilter", "capsfilter");
-    tee = gst_element_factory_make("tee", "tee");
-    queue1 = gst_element_factory_make("queue", "queue1");
-    queue2 = gst_element_factory_make("queue", "queue2");
-    jpegdec = gst_element_factory_make("jpegdec", "jpegdec");
-    videoconvert = gst_element_factory_make("videoconvert", "videoconvert");
-    rtpjpegpay = gst_element_factory_make("rtpjpegpay", "rtpjpegpay");
-    udpsink = gst_element_factory_make("udpsink", "udpsink");
-    autovideosink = gst_element_factory_make("xvimagesink", "xvimagesink");
+    // 创建元素
+    v4l2src       = gst_element_factory_make("v4l2src", "v4l2src");
+    capsfilter    = gst_element_factory_make("capsfilter", "capsfilter");
+    tee           = gst_element_factory_make("tee", "tee");
+    queue_display = gst_element_factory_make("queue", "queue_display");
+    queue_network = gst_element_factory_make("queue", "queue_network");
+    jpegdec       = gst_element_factory_make("jpegdec", "jpegdec");
+    videoconvert  = gst_element_factory_make("videoconvert", "videoconvert");
+    autovideosink = gst_element_factory_make("xvimagesink", "autovideosink");
+    rtpjpegpay    = gst_element_factory_make("rtpjpegpay", "rtpjpegpay");
+    udpsink       = gst_element_factory_make("udpsink", "udpsink");
 
-    if (!v4l2src || !capsfilter || !tee || !queue1 || !queue2 || !jpegdec ||
-        !videoconvert || !rtpjpegpay || !udpsink || !autovideosink) {
+    if (!v4l2src || !capsfilter || !tee || !queue_display || !queue_network ||
+        !jpegdec || !videoconvert || !autovideosink || !rtpjpegpay || !udpsink) {
         g_printerr("Not all elements could be created.\n");
         return -1;
     }
 
-    /* Create the pipeline */
+    // 创建 pipeline
     pipeline = gst_pipeline_new("mjpeg-rtp-pipeline");
     if (!pipeline) {
         g_printerr("Failed to create pipeline.\n");
         return -1;
     }
 
-    /* Configure elements */
+    // 配置 v4l2src
     g_object_set(v4l2src, "device", "/dev/video4", NULL);
 
-    /* udpsink: set destination IP and port (adjust as needed) */
-    g_object_set(udpsink,
-                 "host", "10.0.0.4",
-                 "port", 5000,
-                 "sync", FALSE,
-                 "async", FALSE,
-                 NULL);
-
-    /* Keep queues tiny and leaky (drop old frames) for low latency */
-    g_object_set(queue1, "max-size-buffers", 1, "leaky", 2, NULL);
-    g_object_set(queue2, "max-size-buffers", 1, "leaky", 2, NULL);
-
-    /* Video display: don't sync to clock to reduce latency */
-    g_object_set(autovideosink, "sync", FALSE, NULL);
-
-    /* caps: request MJPEG at 640x480@30 */
+    // 配置 caps: MJPEG 640x480 @30fps
     GstCaps *caps = gst_caps_new_simple(
         "image/jpeg",
         "width", G_TYPE_INT, 640,
@@ -62,67 +49,70 @@ int main(int argc, char *argv[]) {
     g_object_set(capsfilter, "caps", caps, NULL);
     gst_caps_unref(caps);
 
-    /* Add elements to pipeline */
+    // 配置 udpsink
+    g_object_set(udpsink,
+                 "host", "10.0.0.4",
+                 "port", 5000,
+                 "sync", FALSE,
+                 "async", FALSE,
+                 NULL);
+
+    // 队列配置低延迟
+    g_object_set(queue_display, "max-size-buffers", 1, "leaky", 2, NULL);
+    g_object_set(queue_network, "max-size-buffers", 1, "leaky", 2, NULL);
+
+    // 配置本地显示
+    g_object_set(autovideosink, "sync", FALSE, NULL);
+
+    // 配置 RTP MJPEG
+    g_object_set(rtpjpegpay, "mtu", 1400, NULL);  // 分包大小
+
+    // 添加元素到 pipeline
     gst_bin_add_many(GST_BIN(pipeline),
                      v4l2src, capsfilter, tee,
-                     queue1, jpegdec, videoconvert, autovideosink,
-                     queue2, rtpjpegpay, udpsink,
+                     queue_display, jpegdec, videoconvert, autovideosink,
+                     queue_network, rtpjpegpay, udpsink,
                      NULL);
 
-    /* Link: v4l2src -> capsfilter -> tee */
+    // 链接 source -> caps -> tee
     if (!gst_element_link_many(v4l2src, capsfilter, tee, NULL)) {
         g_printerr("Failed to link source -> caps -> tee.\n");
-        gst_object_unref(pipeline);
         return -1;
     }
 
-    /* Request tee src pads and link to queues */
-    GstPad *tee_src_pad1 = gst_element_request_pad_simple(tee, "src_%u");
-    GstPad *tee_src_pad2 = gst_element_request_pad_simple(tee, "src_%u");
-    GstPad *queue1_sink = gst_element_get_static_pad(queue1, "sink");
-    GstPad *queue2_sink = gst_element_get_static_pad(queue2, "sink");
-
-    if (gst_pad_link(tee_src_pad1, queue1_sink) != GST_PAD_LINK_OK ||
-        gst_pad_link(tee_src_pad2, queue2_sink) != GST_PAD_LINK_OK) {
-        g_printerr("Tee could not be linked to queues.\n");
-        gst_object_unref(pipeline);
-        return -1;
-    }
-
-    gst_object_unref(queue1_sink);
-    gst_object_unref(queue2_sink);
-
-    /* Link display branch: queue1 -> jpegdec -> videoconvert -> sink */
-    if (!gst_element_link_many(queue1, jpegdec, videoconvert, autovideosink, NULL)) {
+    // tee -> display 分支
+    GstPad *tee_pad_display = gst_element_request_pad_simple(tee, "src_%u");
+    GstPad *queue_display_sink = gst_element_get_static_pad(queue_display, "sink");
+    gst_pad_link(tee_pad_display, queue_display_sink);
+    gst_object_unref(queue_display_sink);
+    if (!gst_element_link_many(queue_display, jpegdec, videoconvert, autovideosink, NULL)) {
         g_printerr("Failed to link display branch.\n");
-        gst_object_unref(pipeline);
         return -1;
     }
 
-    /* Link network branch: queue2 -> rtpjpegpay -> udpsink */
-    if (!gst_element_link_many(queue2, rtpjpegpay, udpsink, NULL)) {
+    // tee -> network 分支
+    GstPad *tee_pad_network = gst_element_request_pad_simple(tee, "src_%u");
+    GstPad *queue_network_sink = gst_element_get_static_pad(queue_network, "sink");
+    gst_pad_link(tee_pad_network, queue_network_sink);
+    gst_object_unref(queue_network_sink);
+    if (!gst_element_link_many(queue_network, rtpjpegpay, udpsink, NULL)) {
         g_printerr("Failed to link network branch.\n");
-        gst_object_unref(pipeline);
         return -1;
     }
 
-    /* Optional: tune rtpjpegpay (mtu) to control packet size */
-    g_object_set(rtpjpegpay, "mtu", 1400, NULL);
-
-    /* Start playing */
+    // 设置 pipeline 运行
     ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
     if (ret == GST_STATE_CHANGE_FAILURE) {
-        g_printerr("Unable to set the pipeline to the playing state.\n");
-        gst_object_unref(pipeline);
+        g_printerr("Unable to set pipeline to PLAYING.\n");
         return -1;
     }
 
-    /* Wait until error or EOS */
+    // 等待错误或 EOS
     bus = gst_element_get_bus(pipeline);
     msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-                                     static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+                                     (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
-    if (msg != NULL) {
+    if (msg) {
         if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
             GError *err;
             gchar *debug_info;
@@ -136,15 +126,12 @@ int main(int argc, char *argv[]) {
         gst_message_unref(msg);
     }
 
-    /* Cleanup */
+    // 清理
     gst_element_set_state(pipeline, GST_STATE_NULL);
-
-    /* release requested tee pads */
-    gst_element_release_request_pad(tee, tee_src_pad1);
-    gst_element_release_request_pad(tee, tee_src_pad2);
-    gst_object_unref(tee_src_pad1);
-    gst_object_unref(tee_src_pad2);
-
+    gst_element_release_request_pad(tee, tee_pad_display);
+    gst_element_release_request_pad(tee, tee_pad_network);
+    gst_object_unref(tee_pad_display);
+    gst_object_unref(tee_pad_network);
     gst_object_unref(bus);
     gst_object_unref(pipeline);
 
